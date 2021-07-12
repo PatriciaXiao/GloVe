43714517f09ff16eff03537e6b6576ce6682d5db
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-07-12 16:59:33 -0700

    updates

--- process_embedding.py
+++ process_embedding.py
@@ -7,4 +7,7 @@ data = {
     "idx": list(range(len(in_df)+1)), 
     "word": ["<UNK>"] + [str(w).replace('\x01', ' ') for w in list(in_df[0])],
     "count": [0] + list(in_df[1])}
-print(data["count"])
+# print(data["count"])
+
+out_df = pd.DataFrame(data=data)
+out_df.to_csv("vocab.csv", sep="\t", index=None)
