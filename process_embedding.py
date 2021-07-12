a079a7fb1b5220433622ae65e2dbea6e2c982475
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-07-12 16:42:34 -0700

    Update process_embedding.py

--- process_embedding.py
+++ process_embedding.py
@@ -3,5 +3,8 @@ import pandas as pd
 
 in_df = pd.read_csv("political_vocab.txt", sep=" ", header=None)
 
-data = {"idx": list(range(len(in_df)+1)), "word": ["<UNK>"] + [str(w).replace('\x01', ' ') for w in list(in_df[0])]}
-print(data["word"])
+data = {
+    "idx": list(range(len(in_df)+1)), 
+    "word": ["<UNK>"] + [str(w).replace('\x01', ' ') for w in list(in_df[0])],
+    "count": [0] + list(in_df[1])}
+print(data["count"])
