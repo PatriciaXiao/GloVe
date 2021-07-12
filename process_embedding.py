993ef55a66d84ec3f4fee493f55666e9043e8a10
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-07-12 15:04:44 -0700

    updates

---
+++ process_embedding.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd 
+
+in_df = pd.read_csv("political_vocab.txt", sep=" ", header=None)
+
+data = {"idx": list(range(len(in_df)+1)), "word": ["<UNK>"] + [str(w).replace('\x01', ' ') for w in list(in_df[0])]}
+print(data["word"])
